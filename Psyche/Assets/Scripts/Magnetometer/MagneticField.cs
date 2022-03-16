1202a40f385cce5499265215885bf28edb7cac9f
Chris <sukebei@gmail.com>
2022-03-16 13:48:49 -0700

    Magnetometer Update Full Fix, BGM Hack

---
+++ Psyche/Assets/Scripts/Magnetometer/MagneticField.cs
@@ -0,0 +1,200 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MyBox;
+
+public class MagneticField : MonoBehaviour
+{
+    //Construction Details
+    public Vector3 Focus { get; set; }
+    public Vector3 Center { get; set; }
+    public float Eccentricity { get; set; }
+    public float MajorAxis { get; set; }
+    public float MinorAxis { get; set; }
+
+    //GameMechanic Details
+    public bool isAnomally { private get; set; }
+    public float lifeSpan { private get; set; }
+    public float fieldScoreMod { private get; set; }
+    public float alpha { private get; set; }
+    public Vector3 angle2sun { private get; set; }
+    private float currLife = 0.0f;
+    
+
+    private MagnetometerController magController;
+    public SpriteRenderer sprite;
+    public ObjectRotate rotation;
+    public GameObject fluxLineMaster;
+    private List<TrailRenderer> fluxLines;
+    private float messgWait = 0.0f;
+    private float messgMax = 2.0f;
+
+    public void SetMagController(MagnetometerController control)
+    {
+        magController = control;
+    }
+
+    public void ChangeAlpha(float val)
+    {
+        sprite.color = new Color(sprite.color.r, sprite.color.b, sprite.color.b, val);
+        foreach (TrailRenderer line in fluxLines)
+            line.startColor = new Color(line.startColor.r, line.startColor.g, line.startColor.b, val);
+    }
+
+    public void ChangeRotateSpeed(float val)
+    {
+        rotation.SetSpeed(val);
+    }
+
+    public Vector3 GetFieldCenter()
+    {
+        return this.transform.position;
+    }
+
+    public void FixedUpdate()
+    {
+        if (!isAnomally)
+            return;
+        else
+        {
+            currLife += Time.deltaTime;
+            if (currLife < lifeSpan * 0.125f) //fadein
+            {
+                float modalpha = alpha * (currLife / (lifeSpan * 0.125f));
+                ChangeAlpha(modalpha);
+            }
+
+            if (currLife > lifeSpan * 0.75f) //fadeout
+            {
+                float modalpha = alpha * (1 - (currLife - lifeSpan * 0.75f) / (lifeSpan - lifeSpan * 0.75f));
+                ChangeAlpha(modalpha);
+            }
+
+            float posVsSunAngle = Vector3.Angle(angle2sun, this.transform.position.normalized);
+            this.transform.localScale = new Vector3(this.transform.localScale.x, this.transform.localScale.y, MajorAxis * 0.5f + Mathf.Pow(MajorAxis * 4, (posVsSunAngle / 180.0f) * 0.9f ));
+            this.transform.position = this.transform.position.normalized * ((GameRoot.mainAsteroid.GetComponent<CircleCollider2D>().radius * GameRoot.mainAsteroid.transform.localScale.x) - (1 - Mathf.Pow(MajorAxis * 1.5f, (posVsSunAngle / 180.0f) * 0.9f)));
+
+
+            if (currLife > lifeSpan)
+            {
+                magController.SetRingStatus(false);
+                magController.bonusScoreMod -= fieldScoreMod;
+                if (magController.bonusScoreMod < 0.0f)
+                    magController.bonusScoreMod = 0.0f;
+
+                Destroy(this.gameObject);
+            }
+        }
+
+        //if (messgWait > messgMax)
+        //    return;
+        //messgWait += Time.deltaTime;
+    }
+
+
+    public void OnTriggerStay(Collider other)
+    {
+        magController.SetRingStatus(true);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "magnet")
+        {
+            magController.bonusScoreMod += fieldScoreMod;
+            //if (messgWait >= messgMax)
+            //{
+            //    Magnetometer.toolActive = true;
+            //    PopMessageUI.PopUpMessage("Entering Magnetometer Orbit", 4.0f);
+            //    messgWait = 0.0f;
+            //}
+
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "magnet")
+        {
+            magController.SetRingStatus(false);
+            magController.bonusScoreMod -= fieldScoreMod;
+            if (magController.bonusScoreMod < 0.0f)
+                magController.bonusScoreMod = 0.0f;
+            //if (messgWait >= messgMax)
+            //{
+            //    Magnetometer.toolActive = false;
+            //    PopMessageUI.PopUpMessage("Exiting Magnetometer Orbit", 4.0f);
+            //    messgWait = 0.0f;
+            //}
+
+        }
+    }
+
+    public void InitializeFlux()
+    {
+        //how big is is this ring to start?
+        //doesn't have to be perfect just need something to scale with
+        //so I'm taking the Area of a circle to use
+        float fluxArea = Mathf.PI * Mathf.Pow(MajorAxis / 2.0f, 2.0f); //outside ring
+        
+        Debug.Log(this.gameObject.name + " FluxArea: " + fluxArea.ToString("0.0000"));
+        Vector2 dir;
+        GameObject fluxLine;
+        TrailRenderer fluxTrail;
+        fluxLines = new List<TrailRenderer>();
+        int sizeMod = Mathf.CeilToInt( fluxArea / 1000.0f );
+        Debug.Log(this.gameObject.name +  " SizeMod: " + sizeMod.ToString());
+        float timeSizeMod = Mathf.Pow(2, MinorAxis / MajorAxis); //bigger size means longer lasting
+        float timeRotMod = 1.0f / Mathf.Abs(rotation.rotationSpeed); //faster rotation means shorter lasting
+        float timeBaseMax = 0.5f;
+        float timeModMulti = 1.2f;
+        float maxTime = timeBaseMax + (timeModMulti * timeRotMod * timeSizeMod);
+        Debug.Log(this.gameObject.name + " MaxTime: " + maxTime.ToString());
+
+
+        for (int i = 0; i < 10 + sizeMod; i++)
+        {
+            fluxLine = Instantiate(fluxLineMaster);
+            fluxLine.SetActive(true);
+            fluxLine.transform.parent = sprite.transform;
+
+            dir = Random.insideUnitCircle.normalized;
+            fluxLine.transform.localPosition = dir * Random.Range(0.85f, 1.0f) * 2;
+
+            fluxTrail = fluxLine.GetComponent<TrailRenderer>();
+            if (isAnomally)
+            {
+                int val = Random.Range(0,3);
+                switch(val)
+                {
+                    case 0:
+                        fluxTrail.startColor = new Color(Random.Range(0.8f, 1.0f), 0, 0, 1);
+                        fluxTrail.endColor = new Color(Random.Range(0.2f, 0.5f), 0, 0, 0);
+                        break;
+                    case 1:
+                        fluxTrail.startColor = new Color(0, Random.Range(0.8f, 1.0f), 0, 1);
+                        fluxTrail.endColor = new Color(0, Random.Range(0.2f, 0.5f), 0, 0);
+                        break;
+                    case 2:
+                        fluxTrail.startColor = new Color(0, 0, Random.Range(0.8f, 1.0f), 1);
+                        fluxTrail.endColor = new Color(0, 0, Random.Range(0.2f, 0.5f), 0);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                fluxTrail.startColor = new Color(0, 1, 0, 1);
+                fluxTrail.endColor = new Color(0, Random.Range(0.5f, 1.0f), 0, 0);
+
+            }
+
+            fluxTrail.startWidth = Random.Range(1.5f, 5.0f);
+            fluxTrail.time = Random.Range(0.1f, timeBaseMax + (timeModMulti * timeRotMod * timeSizeMod));
+
+            fluxLines.Add(fluxTrail);
+
+        }
+    }
+}
---
+++ Psyche/Assets/Scripts/Magnetometer/MagnetometerController.cs
@@ -0,0 +1,287 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MyBox;
+
+public class MagnetometerController : MonoBehaviour
+{
+    public GameObject fieldObject;
+    public GameObject target; //This gets attached to the player
+    public GameObject effect; //this is attached to the target
+
+    [Header("======= Main Field =========")]
+    [Range(1, 200)]
+    public int pivotDistance = 60;
+    [Range(30, 200)]
+    public float orbitRadius; //periapsis distance also called r1
+    private Transform mainFieldsHolder;
+
+    [Header("======= Anomalies =========")]
+    [MinMaxRange(0, 100, order = 1)]
+    [Tooltip("Chance to spawn a new surface anomally.\n")]
+    public RangedInt spawnChance = new RangedInt(50, 75);
+    private int spawnVal;
+    [MinMaxRange(0.0f, 20.0f, order = 2)]
+    [Tooltip("How long an anomally will appear before disappearing")]
+    public RangedFloat spawnLifetime = new RangedFloat(3.0f, 7.0f);
+    private float currentTime = 0;
+    [MinMaxRange(0.0f, 20.0f, order = 2)]
+    [Tooltip("The time interval (in seconds) between each spawn window")]
+    public RangedFloat spawnInterval = new RangedFloat(3.0f, 7.0f);
+    [MinMaxRange(10.0f, 50.0f)]
+    [Tooltip("Adjusts the ranged of sizes that target anomallies will appear to be.\nThe size of the anomally also plays into how much points they are worth when scanned (smaller size = more points)")]
+    public RangedFloat spawnSizeRange = new RangedFloat(3.0f, 6.0f);
+    [MinMaxRange(0.1f, 3.0f)]
+    [Tooltip("Adjusts the speed that anomalies will spin at.")]
+    public RangedFloat spawnRotSpeed = new RangedFloat(0.5f, 1.0f);
+    [MinMaxRange(0.1f, 1.0f)]
+    [Tooltip("Adjusts the alpha that anomalies will appear at.")]
+    public RangedFloat spawnAlpha = new RangedFloat(0.5f, 0.9f);
+
+    private float intervalVal;
+    private Transform anomaliesHolder;
+
+
+    [Header("======= Scoring =========")]
+    [SerializeField] private float baseScoreMod = 0.1f;
+    [SerializeField] private float bonusScoreAdjust = 1.0f;
+    [ReadOnly]
+    public float bonusScoreMod = 0.0f;
+
+
+    [Header("======= Debug =========")]
+
+    [ReadOnly]
+    [SerializeField] private bool inField = false;
+    public static bool toolActive = false;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anomaliesHolder = transform.Find("Anomalies");
+        mainFieldsHolder = transform.Find("MainFields");
+
+        //Send target object to the player object
+        target.transform.position = GameRoot.player.transform.position;
+        target.transform.parent = GameRoot.player.transform;
+        fieldObject.SetActive(false);
+
+        //Send anomalies holder to asteroid
+        anomaliesHolder.parent = GameRoot.mainAsteroid.transform;
+
+        CreateMainFields();
+        intervalVal = Random.Range(spawnInterval.Min, spawnInterval.Max);
+    }
+
+    private void LateUpdate()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            toolActive = !toolActive;
+            effect.SetActive(toolActive);            
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        currentTime += Time.deltaTime;
+        if (currentTime > intervalVal)
+        {
+            spawnVal = Random.Range(spawnChance.Min, spawnChance.Max);
+            if (Random.value * 100.0f <= spawnVal)
+            {
+                CreateAnomallyField();
+            }
+            intervalVal = Random.Range(spawnInterval.Min, spawnInterval.Max);
+            currentTime = 0;
+        }
+
+
+        if (toolActive)
+        {
+            if(ShipControl.isThrusting || !ShipControl.resources.CanUsePower())
+            {
+                toolActive = false;
+                effect.SetActive(toolActive);
+                return;
+            }
+            else if (inField)
+            {
+                GameRoot._Root.ScoreMagnetometer(Time.deltaTime * (baseScoreMod + bonusScoreMod));
+                ShipControl.resources.UsePower(Constants.Ship.Resources.PowerUse.Magnetometer * Time.deltaTime);
+            }
+        }
+    }
+
+    private void CreateAnomallyField()
+    {
+        GameObject anomally = Instantiate(fieldObject);
+
+        MagneticField anomallyField = anomally.GetComponent<MagneticField>();
+
+        float alpha = Random.Range(spawnAlpha.Min, spawnAlpha.Max);
+        float rotSpeed = Random.Range(spawnRotSpeed.Min, spawnRotSpeed.Max);
+        float major = Random.Range(spawnSizeRange.Min, spawnSizeRange.Max);
+        float minor = Random.Range(spawnSizeRange.Min, spawnSizeRange.Max);
+        float lifetime = Random.Range(spawnLifetime.Min, spawnLifetime.Max);
+
+        float score = 1 - ((alpha - spawnAlpha.Min) / (spawnAlpha.Max - spawnAlpha.Min));
+        score += 1 - ((rotSpeed - spawnRotSpeed.Min) / (spawnRotSpeed.Max - spawnRotSpeed.Min));
+        score += 1 - ((major - spawnSizeRange.Min) / (spawnSizeRange.Max - spawnSizeRange.Min));
+        score += 1 - ((minor - spawnSizeRange.Min) / (spawnSizeRange.Max - spawnSizeRange.Min));
+        score += 1 - ((lifetime - spawnLifetime.Min) / (spawnLifetime.Max - spawnLifetime.Min));
+        score *= bonusScoreAdjust;
+
+        anomally.name = "anomally" + score.ToString("0.000");
+        anomally.transform.parent = anomaliesHolder;
+        anomally.SetActive(true);
+
+        anomallyField.SetMagController(this);
+        rotSpeed *= Random.value > 0.5f ? 1.0f : -1.0f;
+        anomallyField.ChangeRotateSpeed(rotSpeed);
+        anomallyField.Focus = Vector3.zero;
+        anomallyField.Center = Random.insideUnitCircle.normalized * ((GameRoot.mainAsteroid.GetComponent<CircleCollider2D>().radius * GameRoot.mainAsteroid.transform.localScale.x) + major / 4.0f);
+        if (major > minor)
+        {
+            anomallyField.MinorAxis = minor + orbitRadius / 2.0f;
+            anomallyField.MajorAxis = major + pivotDistance / 2.0f;
+        }
+        else
+        {
+            anomallyField.MinorAxis = major + orbitRadius / 2.0f;
+            anomallyField.MajorAxis = minor + pivotDistance / 2.0f;
+        }
+
+        anomallyField.Eccentricity = 0.0f;
+        anomallyField.isAnomally = true;
+        anomallyField.fieldScoreMod = score;
+        anomallyField.lifeSpan = lifetime;
+        anomallyField.alpha = alpha;
+        anomallyField.angle2sun = GameObject.FindGameObjectWithTag("Sun").transform.position.normalized;
+        anomallyField.InitializeFlux();
+//        anomallyField.ChangeAlpha(0.0f);
+
+        FitEllipse(anomallyField);
+
+    }
+
+    private void CreateMainFields()
+    {
+        GameObject main = Instantiate(fieldObject);
+        GameObject frontField = Instantiate(fieldObject);
+        GameObject backField = Instantiate(fieldObject);
+
+        MagneticField mainField = main.GetComponent<MagneticField>();
+        MagneticField sideFields = frontField.GetComponent<MagneticField>();
+
+
+        main.name = "MainField";
+        main.transform.parent = mainFieldsHolder;
+        main.SetActive(true);
+        
+        mainField.SetMagController(this);
+        mainField.ChangeRotateSpeed(2f);
+        mainField.Focus = Vector3.zero; // zero is the position of the asteroid body
+        mainField.Center = -GameObject.FindGameObjectWithTag("Sun").transform.position.normalized * pivotDistance; //move the center away some pivot distance opposite from the sun
+        mainField.Eccentricity = orbitRadius;
+        mainField.isAnomally = false;
+        mainField.fieldScoreMod = 0.0f;
+
+        DefineEllipse(mainField);
+        mainField.InitializeFlux();
+        mainField.ChangeAlpha(0.2f);
+        FitEllipse(mainField);
+
+        //FrontField===========
+        frontField.name = "FrontField";
+        frontField.transform.parent = mainFieldsHolder;
+        frontField.SetActive(true);
+
+        sideFields.SetMagController(this);
+        sideFields.ChangeRotateSpeed(0.5f);
+        sideFields.Focus = Vector3.zero;
+        sideFields.Center = GameObject.FindGameObjectWithTag("Sun").transform.position.normalized * (mainField.Eccentricity * 0.4f); //move towards sun
+        //sideFields.Focus = sideFields.Center + Vector3.Cross(GameObject.FindGameObjectWithTag("Sun").transform.position, Vector3.forward).normalized * mainField.Eccentricity * 0.4f;
+        //sideFields.Center = (mainField.Focus - mainField.Center).normalized * (mainField.Eccentricity * 0.4f);
+        sideFields.MinorAxis = mainField.MinorAxis * 0.6f;
+        sideFields.MajorAxis = mainField.Eccentricity * 0.85f;
+        sideFields.Eccentricity = mainField.Eccentricity;
+        sideFields.isAnomally = false;
+        sideFields.fieldScoreMod = 0.0f;
+
+        //DefineEllipse(sideFields);
+        sideFields.InitializeFlux();
+        sideFields.ChangeAlpha(0.3f);
+
+        FitEllipse(sideFields);
+
+
+        //BackField==============
+        backField.name = "BackField";
+        backField.transform.parent = mainFieldsHolder;
+        backField.SetActive(true);
+
+        sideFields = backField.GetComponent<MagneticField>();
+        sideFields.SetMagController(this);
+        sideFields.ChangeRotateSpeed(-1f);
+        sideFields.Focus = -GameObject.FindGameObjectWithTag("Sun").transform.position.normalized * (mainField.Eccentricity * 0.4f);
+        //sideFields.Center = -GameObject.FindGameObjectWithTag("Sun").transform.position.normalized * (((mainField.MajorAxis - 2 * mainField.Eccentricity) * 0.4f) / 2.0f);
+        sideFields.Center = mainField.Center + mainField.Center.normalized * (mainField.Eccentricity * 0.5f);
+        //sideFields.MinorAxis = mainField.MinorAxis * 0.8f;
+        //sideFields.MajorAxis = (mainField.Focus - mainField.Center).magnitude * 2.0f;
+        sideFields.Eccentricity = mainField.Eccentricity * 0.4f;
+        sideFields.isAnomally = false;
+        sideFields.fieldScoreMod = 0.0f;
+
+        DefineEllipse(sideFields);
+        sideFields.InitializeFlux();
+        sideFields.ChangeAlpha(0.3f);
+        sideFields.MinorAxis += mainField.MinorAxis * 0.1f;
+        FitEllipse(sideFields);
+    }
+
+    public void SetRingStatus(bool val)
+    {
+        inField = val;
+    }
+
+    private void FitEllipse(MagneticField field)
+    {
+        //scaling defined by size we want divided by the size we have
+        float angle = Vector3.SignedAngle(Vector3.right, field.Focus - field.Center, Vector3.forward);
+        Debug.Log("angle: " + angle);
+
+        field.transform.position = field.Center;
+        field.transform.localScale = new Vector3(field.MinorAxis, 1, field.MajorAxis);
+        field.transform.Rotate(Vector3.up , angle);
+    }
+
+    private void DefineEllipse(MagneticField field)
+    {
+        field.MajorAxis = ((field.Focus - field.Center).magnitude + field.Eccentricity) * 2.0f;
+
+        if (field.Center != field.Focus)
+        {
+            field.MinorAxis = (Mathf.Sqrt(field.Eccentricity * ((2 * (field.Focus - field.Center).magnitude) + field.Eccentricity))) * 2.0f;
+        }
+        else
+        {
+            field.MinorAxis = field.MajorAxis;
+        }
+    }
+
+    public Vector3 GetRingCenter()
+    {
+        return mainFieldsHolder.Find("MainField").GetComponent<MagneticField>().Center;
+    }
+
+    public float GetRingSize()
+    {
+        return mainFieldsHolder.Find("MainField").GetComponent<MagneticField>().MajorAxis * 0.4f;
+    }
+
+}
