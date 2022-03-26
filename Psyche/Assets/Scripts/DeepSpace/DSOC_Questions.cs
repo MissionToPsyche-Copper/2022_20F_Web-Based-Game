aec037e01c64ded4c47e13bab365b9775bb4439e
Tanner Efnor <tefnor@asu.edu>
2022-03-25 17:15:45 -0700

    DSOC questions update

--- Psyche/Assets/Scripts/DeepSpace/DSOC_Questions.cs
+++ Psyche/Assets/Scripts/DeepSpace/DSOC_Questions.cs
@@ -32,55 +32,55 @@ public class DSOC_Questions : MonoBehaviour
     public static Questions[] Level2Questions = {
         new Questions() {id = 1,
         question = "When does the Psyche spaceraft stop orbiting the Psyche asteroid?",
-        answer = ""} ,
+        answer = "2026"} ,
         new Questions() {id = 2,
         question = "Provides high-resolution images using filters to discriminate between Psyche's metallic and silicate constituents.",
-        answer = ""} ,
+        answer = "MultiSpectral Imager"} ,
         new Questions() {id = 3,
         question = "What university is the mission led by?",
-        answer = ""} ,
+        answer = "Arizona State University"} ,
         new Questions() {id = 4,
         question = "When was the Psyche asteroid discovered?",
-        answer = ""} ,
+        answer = "1852"} ,
         new Questions() {id = 5,
         question = "What astronomer discovered the Psyche asteroid?",
-        answer = ""}
+        answer = "Annibale de Gasparis"}
     };
 
     public static Questions[] Level3Questions = {
         new Questions() {id = 1,
         question = "How far is the Psyche asteroid from the sun?",
-        answer = ""} ,
+        answer = "93 million miles"} ,
         new Questions() {id = 2,
         question = "How long is a year on Psyche?",
-        answer = ""} ,
+        answer = "5 years"} ,
         new Questions() {id = 3,
         question = "How long is a day on Psyche?",
-        answer = ""} ,
+        answer = "4 hours and 12 minutes"} ,
         new Questions() {id = 4,
         question = "Where is the Psyche asteroid?",
-        answer = ""} ,
+        answer = "Main asteroid belt between Mars and Jupiter"} ,
         new Questions() {id = 5,
         question = "Use the X-band radio telecommunications system to measure Psyche's gravity field to high precision.",
-        answer = ""}
+        answer = "Radio Science"}
     };
 
     public static Questions[] Level4Questions = {
         new Questions() {id = 1,
         question = "What is the length of the Psyche spacecraft?",
-        answer = ""} ,
+        answer = "24.76 meters"} ,
         new Questions() {id = 2,
         question = "What is the width of the Psyche spacecraft?",
-        answer = ""} ,
+        answer = "7.34 meters"} ,
         new Questions() {id = 3,
         question = "Will detect, measure and map Psyche's elemental composition.",
-        answer = ""} ,
+        answer = "Gamma Ray and Neutron Spectrometer"} ,
         new Questions() {id = 4,
         question = "What kind of propellant will the Psyche spacecraft use?",
-        answer = ""} ,
+        answer = "Xenon"} ,
         new Questions() {id = 5,
         question = "How far will the Psyche spacecraft travel?",
-        answer = ""}
+        answer = "1.5 billion miles"}
     };
 
 
